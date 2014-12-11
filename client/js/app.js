c998de7818c3340b9af35656031411bf472d2ab1
Ben McCarthy <ben.mccarthy@autotrader.co.uk>
2014-12-11 22:46:24 +0000

    Added in the pusher libraries and started to hook into the channel on pusher.

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,10 @@
+
 angular.module('myApp', [
-  'ngMaterial'
-]).controller('myController', [function() {
+  'ngMaterial',
+  'pusher-angular'
+]).controller('myController', ['$scope','$pusher',function($scope,$pusher) {
+	var client = new Pusher('62362d4fe56c3f10f0c6');
+    var pusher = $pusher(client);
+	var my_channel = pusher.subscribe('sentibot');
 
 }]);
